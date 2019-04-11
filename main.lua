a567c37bf58d0a8ae26c63be7fe0ec53678a8c29
Paul <liquidream@hotmail.co.uk>
2019-04-11 19:00:57 +0100

    WIP

--- main.lua
+++ main.lua
@@ -4,15 +4,23 @@
 -- See here for some useful Love2D documentation:
 -- https://love2d-community.github.io/love-api/
 
-local total_time_elapsed = 0
 
-function love.draw()
-  local y_offset = 8 * math.sin(total_time_elapsed * 3)
-  love.graphics.print('Edit main.lua to get started!', 400, 300 + y_offset)
-  love.graphics.print('Press Cmd/Ctrl + R to reload.', 400, 316 + y_offset)
-end
+-- This requests a cloud server from Castle
 
-function love.update(dt)
-  total_time_elapsed = total_time_elapsed + dt
-end
+USE_CASTLE_CONFIG = true
+require 'example_server'
+require 'example_client'
+
+
+-- local total_time_elapsed = 0
+
+-- function love.draw()
+--   local y_offset = 8 * math.sin(total_time_elapsed * 3)
+--   love.graphics.print('Edit main.lua to get started!', 400, 300 + y_offset)
+--   love.graphics.print('Press Cmd/Ctrl + R to reload.', 400, 316 + y_offset)
+-- end
+
+-- function love.update(dt)
+--   total_time_elapsed = total_time_elapsed + dt
+-- end
 
