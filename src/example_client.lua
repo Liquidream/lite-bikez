80c6965ea0399d9ee85ce5776b2583b8744df8da
Paul <liquidream@hotmail.co.uk>
2019-06-19 21:07:02 +0100

    Text looks good, but it's "blacked-out" the player lines! 🤨

--- src/example_client.lua
+++ src/example_client.lua
@@ -416,6 +416,10 @@ function drawUI(players)
     -- Draw UI (inc. Player info)
     --pal()
     palt(0,false)
+
+    -- Make text more "readable"
+    printp(0x0330, 0x3123, 0x0330, 0x0, 0x0)
+    printp_color(5, 7, 0)
     
     -- Players    
     if players then
@@ -445,10 +449,10 @@ function drawUI(players)
                 local w,h = sugar.gfx.surface_size(playerPhotos[player.id])
                 sugar.gfx.sspr(0, 0, w, h, x, y,  G, G)
                 -- draw a shortened version of player name (if longer than 1 chars)
-                print(string.sub(player.me.shortname,1,8),
+                pprint(string.sub(player.me.shortname,1,8),
                         x+12-((#player.me.shortname/2)*7), G+6, 51)
                 -- draw player score
-                print(player.score, x+4, G+16, 51)
+                pprint(player.score, x+4, G+16, 51)
             else
                 -- ...otherwise, draw a shape with player col
                 love.graphics.circle('fill', x + 0.5 * G, y + 0.5 * G, 0.5 * G)
@@ -464,19 +468,19 @@ function drawUI(players)
     end
 
     if client.connected then
-        -- Draw our ping
-        print('Ping: ' .. client.getPing(), 2, 2, 24)
+        -- Draw our ping        
+        pprint('Ping: ' .. client.getPing(), 2, 2, 24)
     else
         -- draw background gfx
         drawTitleBG(512, zoom_scale)
 
-        print('Connecting...', GAME_WIDTH/2-35, GAME_HEIGHT/2+50, 24)
+        pprint('Connecting...', GAME_WIDTH/2-35, GAME_HEIGHT/2+50, 24)
     end
 
     -- did we die?
     if homePlayer.dead and homePlayer.killedBy then
         -- display info about our "killer"
-        print('YOU DIED', GAME_WIDTH/2, GAME_HEIGHT/2, 51)
+        pprint('YOU DIED', GAME_WIDTH/2, GAME_HEIGHT/2, 51)
         local msg = ""
         if homePlayer.killedBy > 0 then
             if homePlayer.killedBy ~= homePlayer.id then
@@ -487,7 +491,7 @@ function drawUI(players)
         else
             msg = "You hit a wall!"
         end
-        print(msg, GAME_WIDTH/2-(#msg/2*4), GAME_HEIGHT/2+20, 51)
+        pprint(msg, GAME_WIDTH/2-(#msg/2*4), GAME_HEIGHT/2+20, 51)
 
     end
 
@@ -497,12 +501,18 @@ function drawUI(players)
         for i=1,share.messageCount do
             local msg = share.messages[i]
             local ourMsg = msg.taggedIds[1]==homePlayer.id or msg.taggedIds[2]==homePlayer.id
-            print(msg.text, GAME_WIDTH-165, GAME_HEIGHT-20-yOff+(i*9), ourMsg and msg.col or msg.col-1)
+            pprint(msg.text, GAME_WIDTH-165, GAME_HEIGHT-20-yOff+(i*9), ourMsg and msg.col or msg.col-1)
         end
     end
 
-    print('FPS: ' .. love.timer.getFPS(), 10, GAME_HEIGHT-20, 51)
+    
+    pprint('FPS: ' .. love.timer.getFPS(), 10, GAME_HEIGHT-20, 51)
 
+
+    -- Reset pretty print 
+    -- (otherwise it affects the drawing of players)
+    printp()
+    printp_color()
 
     -- reset trans again
     palt(0,true)
